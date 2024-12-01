2f7c657ea9e9f7d799c7dedf6e7aed9f2c00e01b
mablazej <145717295+DucksForLife@users.noreply.github.com>
2024-12-01 18:57:53 +0100

    frontend.py
    simple input n output before merg with agent

--- sss.py
+++ sss.py
@@ -1,21 +1,26 @@
 import streamlit as st
+import math
 
 # Title of the app
-st.title("My First Streamlit App")
+st.title("Pilot Pass")
 
 # Displaying some text
-st.write("Hello, welcome to this basic Streamlit app!")
+st.write("See how easy is to guess your password")
 
-# Adding a slider
-slider_value = st.slider("Select a number", 0, 100)
-st.write(f"You selected: {slider_value}")
+#input boxes
+st.subheader("Enter Your Personal Details")
+name = st.text_input("Your Name")
+surname = st.text_input("Your Surname")
+dob = st.date_input("Date of Birth")
 
-# Adding a button
-if st.button("Click Me"):
-    st.write("You clicked the button!")
+#conformation button and input data conformation
+if st.button("submit"):
+    st.subheader("Entered Details:")
+    st.write(f"**Name:** {name}")
+    st.write(f"**Surname:** {surname}")
+    st.write(f"**Date of Birth:** {dob}")
 
-# Displaying an input text box
-user_input = st.text_input("Enter your name")
-if user_input:
-    st.write(f"Hello, {user_input}!")
+#thats it for now, the rest is merging of fron n back end to show results of external py code
+#i think that we should show the list of guessed passwords, and make it like a psa to make stronger passwords
+#still looking into how to make a seperet tab for information about system o7
 
